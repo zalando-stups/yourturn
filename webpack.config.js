056c40e10bb6305e43ea4e6e32a0488102312a70
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-03-16 12:01:43 +0100

    support hot loading of react components

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './src/app/bootstrap.jsx',
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/app/bootstrap.jsx',
+    ],
     output: {
-        filename: './dist/bundle.js'       
+        path: __dirname + '/dist/',
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    resolve: {
+        extensions: ['', '.js', '.jsx']
     },
     module: {
         loaders: [
-            // { test: /\.jsx$/, exclude: /node_modules/, loaders: ['babel-loader', 'jsx-loader'] }
-            { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader' },
+            { test: /\.jsx$/, exclude: /node_modules/, loaders: ['react-hot', 'babel'] },
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
         ]
     }
