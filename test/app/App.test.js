ec4b2df00ead9011cd8e32b285cb7043d40ebddb
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-03-16 17:32:39 +0100

    basic test case working

---
+++ test/app/App.test.js
@@ -0,0 +1,11 @@
+describe('The test test', () => {
+    it('should work', () => {
+        expect(true).to.equal(true);
+    });
+});
+
+// describe('The test test', function() {
+//     it('should work', function() {
+//         expect(true).to.equal(true);
+//     });
+// });
