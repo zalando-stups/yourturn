9636a036ce5172386b06362088fb6925ea48bd0f
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-03-16 12:28:40 +0100

    less compilation

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,23 +1,58 @@
-var gulp = require( 'gulp' ),
-    gutil= require( 'gulp-util' ),
-    eslint = require( 'gulp-eslint' ),
-    webpack = require( 'webpack' );
+var gulp = require('gulp'),
+    gutil= require('gulp-util'),
+    eslint = require('gulp-eslint'),
+    less = require('gulp-less'),
+    vendorprefix = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync'),
+    webpack = require('webpack');
 
-gulp.task( 'pack', function( done ) {
+gulp.task('pack', function(done) {
     webpack(
-        require( './webpack.config.js' ),
-        function( err, data ) {
-            if ( err ) {
-                throw new gutil.PluginError( 'webpack', err );
+        require('./webpack.config.js'),
+        function(err, data) {
+            if (err) {
+                throw new gutil.PluginError('webpack', err);
             }
-            gutil.log( '[webpack]', data.toString() );
+            gutil.log('[webpack]', data.toString());
         });
 });
 
-gulp.task( 'lint', function() {
-    gulp
-        .src([ 'src/app/**/*.jsx', 'src/app/**/*.js' ])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+gulp.task('sync', function() {
+    browserSync({
+        files: 'css/**/*.css',
+        server: {
+            baseDir: './dist'
+        }
+    });
 });
+
+gulp.task('lint', function() {
+    return gulp
+            .src(['src/app/**/*.jsx', 'src/app/**/*.js'])
+            .pipe(eslint())
+            .pipe(eslint.format())
+            .pipe(eslint.failAfterError());
+});
+
+gulp.task('watch:js', function() {
+    return  gulp.watch([
+                'src/app/**/*.jsx',
+                'src/app/**/*.js'
+            ],
+            ['lint']);
+});
+
+gulp.task('less', function() {
+    return gulp
+            .src('src/asset/less/**/*.less')
+            .pipe(less())
+            .pipe(vendorprefix())
+            .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('watch:less', function() {
+    return gulp.watch('src/asset/less/**/*.less', ['less', 'sync']);
+});
+
+gulp.task('watch', ['watch:less', 'watch:js']);
+gulp.task('default', ['watch']);
--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class App extends React.Component {
     render() {
-        return <h1>HELLO WORLD</h1>;
+        return <h1>HELLO JÖRG</h1>;
     }
 };
