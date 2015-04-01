f9b23cb59218f50bd7a9fc34aaf8e1992ab75f50
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-04-01 17:39:12 +0200

    add server that generates environment configuration

---
+++ server/yourturn.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+    server = express(),
+    fs = require('fs'),
+    index = fs.readFileSync('./index.html');
+
+server.use('/dist', express.static('dist'));
+
+function generateEnv() {
+    var env = '';
+    for( key in process.env ) {
+        if (process.env.hasOwnProperty(key)) {
+            if (key.indexOf( 'YTENV_' ) === 0 ) {
+                env += '' + key + '="' + process.env[key] + '";\n';
+            }
+        }
+    }
+    return env;
+}
+
+function writeEnv() {
+    var env = generateEnv();
+    fs.writeFileSync('./dist/env.js', env );
+}
+
+writeEnv();
+setInterval( writeEnv, 1000 * 60 * 30 ); // write this every 30 minutes
+
+server.get('/*', function(req, res) {
+    res
+        .append('Content-Type', 'text/html')
+        .status(200)
+        .send(index);
+});
+
+server.listen(8080);
