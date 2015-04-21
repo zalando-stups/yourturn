4b18baeed4facd50ea94e264aafedd50e4d1cfaa
Emanuel Lauria <emanuel.lauria@gmail.com>
2015-04-21 17:33:06 +0200

    #35 removed unnecessary dependencies from version-list

--- client/lib/application/src/version-list/application-version.js
+++ client/lib/application/src/version-list/application-version.js
@@ -1,12 +1,6 @@
 import BaseView from 'common/src/base-view';
 import Template from './application-version.hbs';
 import Flux from 'application/src/flux';
-import FetchResult from 'common/src/fetch-result';
-import ErrorTpl from 'common/src/error.hbs';
-import Placeholder from '../detail/placeholder.hbs';
-import Markdown from 'common/src/markdown';
-
-import 'common/asset/scss/application/application.scss';
 
 class AppVersion extends BaseView {
     constructor( props ) {
