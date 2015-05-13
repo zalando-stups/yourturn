59f4c228b9bac71774084fc70e4038e7cba43291
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-05-13 11:41:45 +0200

    resource actions tests

--- client/lib/common/test/bootstrap.js
+++ client/lib/common/test/bootstrap.js
@@ -8,4 +8,5 @@ import './notification/notification-store.test';
 import './oauth/oauth-store.test';
 import './oauth/oauth-actions.test';
 import './search/search-store.test';
+import './resource/resource-actions.test';
 import './resource/resource-store.test';
