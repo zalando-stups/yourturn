2b93df09ce3c2bf9e80046cd0472f22cb73c1782
Emanuel Lauria <emanuel.lauria@gmail.com>
2015-05-13 17:55:01 +0200

    extended tests

--- client/lib/resource/test/resource-form.test.js
+++ client/lib/resource/test/resource-form.test.js
@@ -1,11 +1,17 @@
-/* globals expect */
+/* globals expect, sinon, Promise */
 import {Flummox} from 'flummox';
 import ResourceStore from 'common/src/data/resource/resource-store';
 import ResourceActions from 'common/src/data/resource/resource-actions';
 import Form from 'resource/src/resource-form/resource-form';
 
 const RES = 'resource',
-    RES_ID = 'sales_order';
+    RES_ID = 'sales_order',
+    TEST_RES = {
+        id: 'sales_order',
+        name: 'Sales Order',
+        description: 'Sales Orders',
+        resource_owners: ['employees']
+    };
 
 class MockFlux extends Flummox {
     constructor() {
@@ -18,10 +24,14 @@ class MockFlux extends Flummox {
 
 describe('The resource form view', () => {
     var flux,
+        actionSpy,
         form;
 
     beforeEach(() => {
         flux = new MockFlux();
+        actionSpy = sinon.stub(flux.getActions(RES), 'saveResource', function () {
+            return Promise.resolve();
+        });
     });
 
 
@@ -51,6 +61,26 @@ describe('The resource form view', () => {
         it('should not have a placeholder', () => {
             flux.getStore(RES).beginFetchResource(RES_ID);
             expect(form.$el.find('.u-placeholder').length).to.equal(0);
+        });
+
+        it('should display the available symbol', () => {
+            flux.getStore(RES).receiveResource(TEST_RES);
+            let $available = form.$el.find('[data-block="available-symbol"]').first();
+            expect($available.length).to.equal(1);
+        });
+
+        it('should disable the ID input', () => {
+            flux.getStore(RES).receiveResource(TEST_RES);
+            let $input = form.$el.find('[data-block="id-input"]').first();
+            expect($input.is(':disabled')).to.be.true;
+        });
+
+        it('should call the correct action', () => {
+            flux.getStore(RES).receiveResource(TEST_RES);
+            let $input = form.$el.find('[data-block="name-input"]').first();
+            $input.val('test');
+            form.$el.find('form').submit();
+            expect(actionSpy.calledOnce).to.be.true;
         });
 
     });
