fab785eda6aa30125a4be89dde3465a1a009dbe4
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-05-29 10:43:06 +0200

    #143 update pierone mock

--- server/mocks/5007-pierone.js
+++ server/mocks/5007-pierone.js
@@ -1,9 +1,16 @@
 var express = require('express'),
     server = express();
 
+var scmSource = {
+  url: 'git:git@github.com:zalando-stups/pierone.git',
+  revision: 'cd768599e1bb41c38279c26254feff5cf57bf967',
+  author: 'npiccolotto',
+  status: 'M package.json'
+};
+
 var images = [{
     name: 'stups/yourturn',
-    tag: '0.22-SNAPSHOT'
+    tag: '1.0'
 }];
 
 /** enable cors */
@@ -11,6 +18,18 @@ server.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
+});
+
+server.get('/teams/:team/artifacts/:artifact/tags/:tag/scm-source', function(req, res) {
+    if (req.params.team === 'stups' &&
+        req.params.artifact === 'kio' &&
+        req.params.tag === '1.0') {
+        return res
+                .status(200)
+                .type('json')
+                .send(scmSource);
+    }
+    res.status(404).send();
 });
 
 server.get('/v1/search', function(req,res) {
