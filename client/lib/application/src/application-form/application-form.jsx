04697833fa0775c6c608a1009f7037a70d97b996
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-06-08 17:57:00 +0200

    #162 app form stub

---
+++ client/lib/application/src/application-form/application-form.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ApplicationForm extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return <div>hello</div>
+    }
+}
+
+export default ApplicationForm;
