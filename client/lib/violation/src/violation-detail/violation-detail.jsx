7df77f53740c9d699f48df8b1277e0ef1f52788f
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-08-10 16:43:47 +0200

    #240 new fullstop api

--- client/lib/violation/src/violation-detail/violation-detail.jsx
+++ client/lib/violation/src/violation-detail/violation-detail.jsx
@@ -6,17 +6,28 @@ class ViolationDetail extends React.Component {
     constructor(props) {
         super();
         this.stores = {
-            fullstop: props.flux.getStore('fullstop')
+            fullstop: props.flux.getStore('fullstop'),
+            user: props.globalFlux.getStore('user')
         };
+        this.actions = props.flux.getActions('fullstop');
+    }
+
+    resolveViolation(violation, message) {
+        return this
+                .actions
+                .resolveViolation(violation.id, message);
     }
 
     render() {
         let {violationId} = this.props,
+            accounts = this.stores.user.getUserCloudAccounts().map(a => a.id),
             violation = this.stores.fullstop.getViolation(violationId);
         return <div className='violationDetail'>
                     <Violation
                         flux={this.props.flux}
-                        autoFocus={violation && violation.comment != null}
+                        editable={accounts.indexOf(violation.account_id) >= 0}
+                        onResolve={this.resolveViolation.bind(this)}
+                        autoFocus={this.props.autoFocus || (violation && violation.comment != null)}
                         violation={violation} />
                 </div>;
     }
@@ -24,6 +35,7 @@ class ViolationDetail extends React.Component {
 ViolationDetail.displayName = 'ViolationDetail';
 ViolationDetail.propTypes = {
     violationId: React.PropTypes.string.isRequired,
-    flux: React.PropTypes.object.isRequired
+    flux: React.PropTypes.object.isRequired,
+    globalFlux: React.PropTypes.object.isRequired
 };
 export default ViolationDetail;
