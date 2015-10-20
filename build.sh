6c26a954486f9126052731a9f6c972b6c60b468a
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-10-20 17:19:31 +0200

    More comfortable build script

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+if [ -z "$DEFAULT_DOCKER_REGISTRY" ]
+then
+    echo "WARN: No default docker registry set."
+fi
+
+if [ -z "$1" ]
+then
+    echo "No version supplied, aborting."
+    exit 1
+fi
+
 npm install
 cd client
 gulp build
 cd ..
-docker build -t $1 . && docker push $1
+docker build -t "$DEFAULT_DOCKER_REGISTRY/stups/yourturn:$1" . && docker push "$DEFAULT_DOCKER_REGISTRY/stups/yourturn:$1"
