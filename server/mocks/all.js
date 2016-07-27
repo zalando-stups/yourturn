df262bf3fd94c6f6a40305d57824c96dcf7f616e
Norman Eremic <norman.eremic@zalando.de>
2016-07-27 15:09:30 +0200

    #486 frist step

--- server/mocks/all.js
+++ server/mocks/all.js
@@ -9,6 +9,7 @@ var kio = require('./5000-kio'),
     fullstop = require('./5008-fullstop'),
     user = require('./5009-user'),
     magnificent = require('./5010-magnificent');
+    alice = require('./5011-alice');
 
 kio.listen(5000);
 console.log('kio on 5000');
@@ -32,3 +33,5 @@ user.listen(5009);
 console.log('user on 5009');
 magnificent.listen(5010);
 console.log('magnificent on 5010');
+alice.listen(5011);
+console.log('alice on 5011');
