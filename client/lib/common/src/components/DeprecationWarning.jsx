c25ad375c01f9a483e11cff02fed2732c0eb7140
Andre Hartmann <andre.hartmann@zalando.de>
2017-07-04 11:43:22 +0200

    #534 remove dead code

--- client/lib/common/src/components/DeprecationWarning.jsx
+++ client/lib/common/src/components/DeprecationWarning.jsx
@@ -1,41 +1,22 @@
 import React from 'react';
-import Icon from 'react-fa';
 
 class DeprecationWarning extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {show: true};
-        this.handleDismiss = this.handleDismiss.bind(this);
-    }
-
-    handleDismiss() {
-        this.setState({show:false});
-    }
-
 
     render() {
         return (
-            this.state.show ?
-                <div style={{backgroundColor: '#f08532'}}>
-                    <div style={{display:'flex'}}>
-                        <h4 style={{color: '#FFF', margin: '20px'}}>
-                            Please note that the GitHub approval flow obsoletes Kio versions, i.e. you don't have to maintain version information in Kio anymore. The corresponding API endpoints will be disabled on July 31st, 2017.
-                        </h4>
-                        {this.props.dismissable ?
-                            <div onClick={this.handleDismiss} >
-                                <Icon fixedWidth name='times' />
-                            </div> : null}
-                    </div>
-                </div> : null
+            <div style={{backgroundColor: '#f08532'}}>
+                <div style={{display: 'flex'}}>
+                    <h4 style={{color: '#FFF', margin: '20px'}}>
+                        Please note that the GitHub approval flow obsoletes Kio versions, i.e. you don't have to
+                        maintain version information in Kio anymore. The corresponding API endpoints will be disabled on
+                        July 31st, 2017.
+                    </h4>
+                </div>
+            </div>
         )
     }
 }
 
 DeprecationWarning.displayName = 'DeprecationWarning';
-
-DeprecationWarning.propTypes = {
-    dismissable: React.PropTypes.bool
-};
 
 export default DeprecationWarning;
--- client/lib/yourturn/src/app.jsx
+++ client/lib/yourturn/src/app.jsx
@@ -5,35 +5,23 @@ import DeprecationWarning from '../../common/src/components/DeprecationWarning.j
 
 class YourTurn extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {show: true};
-        this.dismiss = this.dismiss.bind(this);
-    }
-
-    dismiss() {
-        this.setState({show:false});
-    }
-
     render() {
-    return (<div className='yourturn'>
-                <NotificationBar />
-        <div className='grid with-gutter'>
-                    <div className='grid-col col-1-4'>
-                        <Sidebar
-                            activeRoute={this.props.location.pathname} />
-                    </div>
-                    <div className='grid-col'>
-                        {this.props.location.pathname === '/' ?
-                            <DeprecationWarning dismissable={false} /> : null
-                        }
-                        <div className='yourturn-view'>
-                            {this.props.children}
-                        </div>
+        return (<div className='yourturn'>
+            <NotificationBar />
+            <div className='grid with-gutter'>
+                <div className='grid-col col-1-4'>
+                    <Sidebar activeRoute={this.props.location.pathname} />
+                </div>
+                <div className='grid-col'>
+                    {this.props.location.pathname === '/' ?
+                        <DeprecationWarning /> : null
+                    }
+                    <div className='yourturn-view'>
+                        {this.props.children}
                     </div>
                 </div>
-            </div>)
+            </div>
+        </div>)
     }
 }
 
