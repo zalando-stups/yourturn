4ed9d0d35f262b1eaa42165c6864ee0e183c4c19
Andre Hartmann <andre.hartmann@zalando.de>
2017-07-04 11:57:15 +0200

    #534 try to fix linter errors

--- client/lib/common/src/components/DeprecationWarning.jsx
+++ client/lib/common/src/components/DeprecationWarning.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 
-class DeprecationWarning extends React.Component {
-
-    render() {
-        return (
-            <div style={{backgroundColor: '#f08532'}}>
-                <div style={{display: 'flex'}}>
-                    <h4 style={{color: '#FFF', margin: '20px'}}>
-                        Please note that the GitHub approval flow obsoletes Kio versions, i.e. you don't have to
-                        maintain version information in Kio anymore. The corresponding API endpoints will be disabled on
-                        July 31st, 2017.
-                    </h4>
-                </div>
+const DeprecationWarning = () => {
+    return (
+        <div style={{backgroundColor: '#f08532'}}>
+            <div style={{display: 'flex'}}>
+                <h4 style={{color: '#FFF', margin: '20px'}}>
+                    Please note that the GitHub approval flow obsoletes Kio versions, i.e. you don't have to
+                    maintain version information in Kio anymore. The corresponding API endpoints will be disabled on
+                    July 31st, 2017.
+                </h4>
             </div>
-        )
-    }
-}
+        </div>
+    )
+};
 
 DeprecationWarning.displayName = 'DeprecationWarning';
 
