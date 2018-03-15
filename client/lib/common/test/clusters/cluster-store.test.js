3f25d15e824b73ce1c00dfb30ace9130422ea7b2
Dmytro Bukhanets <dmytro.bukhanets@zalando.de>
2018-03-15 17:23:14 +0100

    #275 clean code

--- client/lib/common/test/clusters/cluster-store.test.js
+++ client/lib/common/test/clusters/cluster-store.test.js
@@ -6,56 +6,56 @@ import * as Getter from 'common/src/data/clusters/clusters-getter';
 import FetchResult from 'common/src/fetch-result';
 
 var clusters = [
-    {
-      'alias': 'playground',
-      'environment': 'playground',
-      'id': 'c-1'
-    },
-    {
-      'alias': 'stups-test',
-      'environment': 'test',
-      'id': 'c-2'
-    },
-    {
-      'alias': 'stups',
-      'environment': 'production',
-      'id': 'c-3'
-    },
-    {
-      'alias': 'overarching',
-      'environment': 'production',
-      'id': 'c-4'
-    },
-    {
-      'alias': 'db',
-      'environment': 'production',
-      'id': 'c-5'
-    },
-    {
-      'alias': 'fashion-store',
-      'environment': 'production',
-      'id': 'c-6'
-    },
-    {
-      'alias': 'fashion-store-test',
-      'environment': 'test',
-      'id': 'c-7'
-    },
-    {
-      'alias': 'distributed-commerce',
-      'environment': 'production',
-      'id': 'c-8'
-    },
-    {
-      'alias': 'distributed-commerce-test',
-      'environment': 'test',
-      'id': 'c-9'
-    },
-    {
-      'alias': 'teapot',
-      'environment': 'test',
-      'id': 'c-10'
-    }  ];
+  {
+    'alias': 'st',
+    'environment': 'pg',
+    'id': 'c-1'
+  },
+  {
+    'alias': 'st-t',
+    'environment': 't',
+    'id': 'c-2'
+  },
+  {
+    'alias': 'st',
+    'environment': 'production',
+    'id': 'c-3'
+  },
+  {
+    'alias': 'ovra',
+    'environment': 'production',
+    'id': 'c-4'
+  },
+  {
+    'alias': 'database',
+    'environment': 'production',
+    'id': 'c-5'
+  },
+  {
+    'alias': 'f-store',
+    'environment': 'production',
+    'id': 'c-6'
+  },
+  {
+    'alias': 'f-store-test',
+    'environment': 't',
+    'id': 'c-7'
+  },
+  {
+    'alias': 'dist',
+    'environment': 'production',
+    'id': 'c-8'
+  },
+  {
+    'alias': 'dist-test',
+    'environment': 't',
+    'id': 'c-9'
+  },
+  {
+    'alias': 'tp',
+    'environment': 't',
+    'id': 'c-10'
+  }  ];
 
 describe('The redux cluster store', () => {
     it.skip('should set a pending fetch result for clusters', () => {
