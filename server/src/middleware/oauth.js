26ada0d49ddbe0be77a9a509d8abed9a3e173030
Muhammed Emre Konar <muhammed.emre.konar@zalando.de>
2019-02-05 14:36:34 +0100

    logging added

--- server/src/middleware/oauth.js
+++ server/src/middleware/oauth.js
@@ -13,6 +13,7 @@ function sendGenericError(res) {
  * OAuth 2 middleware.
  */
 module.exports = function(req, res, next) {
+  console.log("oauth.js BPI", BPI);
   // if there is no token, respond with 401
   if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) {
     return sendGenericError(res);
@@ -21,20 +22,26 @@ module.exports = function(req, res, next) {
   var header = req.headers.authorization,
     token = header.substring("Bearer ".length);
 
+  console.log("oauth.js token", token);
+
   // verify token
   request
     .post(process.env.YTENV_OAUTH_TOKENINFO_URL)
     .accept("json")
-    .set({ Authorization: `Bearer ${req.body.access_token}` })
+    .set({
+      Authorization: token,
+      "content-type": "application/x-www-form-urlencoded"
+    })
     .send({
       business_partner_id: BPI
     })
     .then(tokeninfo => {
+      console.log("oauth.js tokeninfo", tokeninfo);
       req.tokeninfo = tokeninfo.body;
       if (tokeninfo.body.realm === "/employees" || tokeninfo.body.realm === "/services") {
         return next();
       }
       return sendGenericError(res);
     })
-    .catch(err => sendGenericError(res));
+    .catch(err => {console.log("oauth.js tokeninfo", tokeninfo); sendGenericError(res)});
 };
--- server/src/routes/tokeninfo.js
+++ server/src/routes/tokeninfo.js
@@ -4,20 +4,29 @@ var winston = require("winston"),
 var BPI = process.env.BUSINESS_PARTNER_ID;
 
 function info(req, res) {
+  console.log("/tokeninfo req body", req.body);
+  console.log("BPI", BPI);
+
   request
     .post(process.env.YTENV_OAUTH_TOKENINFO_URL)
     .accept("json")
-    .set({ Authorization: `Bearer ${req.body.access_token}` })
+    .set({
+      Authorization: `Bearer ${req.body.access_token}`,
+      "content-type": "application/x-www-form-urlencoded"
+    })
     .send({
       business_partner_id: BPI
     })
-    .then(response =>
+    .then(response => {
+      console.log("/tokeninfo response", response);
       res
         .status(200)
         .type("json")
         .send(response.text)
+    }
     )
     .catch(err => {
+      console.log("/tokeninfo error", err);
       if (err.status !== 400) {
         // log error on tokeninfo only if it's not
         // because of an invalid token
