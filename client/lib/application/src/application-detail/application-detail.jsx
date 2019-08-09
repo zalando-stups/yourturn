00c876f208e69a218ed99996d0f63c58a4d7a70f
Dmytro Bukhanets <zedd.malam@gmail.com>
2019-08-09 14:27:22 +0200

    Update application-detail.jsx

--- client/lib/application/src/application-detail/application-detail.jsx
+++ client/lib/application/src/application-detail/application-detail.jsx
@@ -19,7 +19,15 @@ export default function ApplicationDetail({applicationId, application, editable,
     }
 
     return <div className='applicationDetail'>
-                <h1>{application.name || applicationId}</h1>
+                <h1>
+                    {YTENV_APPLICATION_BASE_URL && (
+                        <a title={YTENV_APPLICATION_LINK_TITLE} style={{textDecoration: "none", backgroundImage: "none"}} href={YTENV_APPLICATION_BASE_URL + applicationId}>
+                            <svg width="40" height="100%" viewBox="0 0 24 24">
+                                <path fill="#000000" d="M19,4C20.11,4 21,4.9 21,6V18A2,2 0 0,1 19,20H5C3.89,20 3,19.1 3,18V6A2,2 0 0,1 5,4H19M19,18V8H5V18H19Z" />
+                            </svg>
+                        </a>)}
+                    {application.name || applicationId}
+                </h1>
                 <div className='btn-group'>
                     <Link
                         to={Routes.appList(LINK_PARAMS)}
